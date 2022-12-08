fbf7c9bab14a492d389efbcd5f86d1ef59bd7114
siham394 <sihamissa94@gmail.com>
2022-12-08 11:14:37 -0800

    betty writing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - Entry point
  *
@@ -7,6 +8,7 @@
  */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
         return (0);
