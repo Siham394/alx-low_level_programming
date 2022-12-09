537005e88cb8535b52923e12c1bb7315c0cf4e50
siham394 <sihamissa94@gmail.com>
2022-12-09 12:05:29 -0800

    print digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits upto 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	printf("\n");
+
+	return (0);
+}
