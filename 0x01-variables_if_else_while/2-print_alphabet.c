ce42b44fc44c28424e8e397bb208de984f654fc7
siham394 <sihamissa94@gmail.com>
2022-12-09 11:27:02 -0800

    lowercase putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase letters
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	{
+		putchar('\n');
+	}
+	return (0);
+}
