5c48ea5a102b52de0b5dbc625929e3397bccc5e6
siham394 <sihamissa94@gmail.com>
2022-12-09 11:39:12 -0800

    lower upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints lower and upper case letter
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+
+	putchar('\n');
+}
