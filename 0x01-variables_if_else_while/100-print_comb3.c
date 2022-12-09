6fd5980a4b877bbb79577679ade0c5b6c5fcf155
siham394 <sihamissa94@gmail.com>
2022-12-09 15:29:58 -0800

    decimal

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,21 @@
  */
 int main(void)
 {
-	int a,b;
-	a=1;
-	b=1;
-	if (a = b)
-		putchar((a % 10) + '0');
-	else if (a < b)
-		putchar((b % 10) + 'o');
-	else if(a > b)
-		putchar((a % 10) + 'o');
-	a++;
-		b++;
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '8' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
