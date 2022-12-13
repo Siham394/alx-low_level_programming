a16bf4f8184d0edcffcc1a8c4616029cf528b3a5
siham394 <sihamissa94@gmail.com>
2022-12-13 15:36:52 -0800

    y

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,13 @@
 
 /**
  * print_alphabet - check the code
- * Return: Always 0.
- */
+ *
+ **/
 void print_alphabet(void)
 {
-	char x;
-	for (x = 'a'; x <= 'z'; x++)
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(0);
 	_putchar('\n');
 }
