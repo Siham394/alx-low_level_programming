cdc1f48b5c65deba5eaba14f029352bc6c969fd3
siham394 <sihamissa94@gmail.com>
2022-12-14 07:46:00 -0800

    add ,

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,9 +22,9 @@ void print_times_table(int n)
 			product = (row * column);
 			if (column == 0)
 				_putchar('0' + product);
-			else 
+			else
 			{
-				_putchar('.');
+				_putchar(',');
 				_putchar(' ');
 				if (product <= 9)
 				{
